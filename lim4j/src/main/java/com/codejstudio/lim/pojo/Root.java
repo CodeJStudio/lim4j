fb2df111555a69f7805a205edfaac0cdad0aec8c
CodeJStudio <codejstudio@hotmail.com>
2024-05-24 01:12:40 +0800

    Update Root.java

--- lim4j/src/main/java/com/codejstudio/lim/pojo/Root.java
+++ lim4j/src/main/java/com/codejstudio/lim/pojo/Root.java
@@ -54,7 +54,7 @@ public final class Root implements Serializable {
 	private static final String PROPERTIES_FILENAME_JAXB = "jaxb-substantial";
 
 	@XmlAttribute(name = "version")
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "2.0";
 
 
 	/* variables */
